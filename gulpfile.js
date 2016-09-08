16542bbb328d2e1b7674824b61baff75568ec61b
tiagoos <contato.tiagoos@gmail.com>
2016-09-07 23:27:10 -0300

    inicio / falta ajustar gulp

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var gulp = require("gulp"),
 
 var dirs = {
     //src
+    
     Html:['./app/**/*.html','.app/libs'],
     Css: ['./app/**/*.scss','!.app/lib'],
     Js: ['./app/assets/js/*.js','!.app/lib'],
